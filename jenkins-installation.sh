650203d8cbcac89ac964e117a4352cb1de8e3cb4
Bachedevopsengineer <106281150+Bachedevopsengineer@users.noreply.github.com>
2023-07-24 20:16:43 -0400

    Create jenkins-installation.sh

---
+++ jenkins-installation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#JENKINS INSTALLATION SCRIPT
+
+#First install prerequisites like java and git
+sudo apt update -y
+sudo apt install openjdk-17-jre git-all wget vim -y
+#java -version
+
+#Add jenkins library and install jenkins
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
